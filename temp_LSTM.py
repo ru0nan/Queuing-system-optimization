3ab495715bf6e71293c57921e3398dc629a745c9
ru0nan <36369871+ru0nan@users.noreply.github.com>
2018-04-14 20:09:45 +0800

    LSTM forecast（shampoo）
    The code is not original
    source page：https://machinelearningmastery.com/multi-step-time-series-forecasting-long-short-term-memory-networks-python/

--- temp_LSTM.py
+++ temp_LSTM.py
@@ -128,7 +128,7 @@ def plot_results(predicted_data, true_data, filename):
 def plot_results_multiple(predicted_data, true_data, prediction_len):
     fig = plt.figure(facecolor='white')
     ax = fig.add_subplot(111)
-    ax.plot(true_data, label='True Data')
+    ax.plot(true_data, label='Reality')
     #Pad the list of predictions to shift it in the graph to it's correct start
     for i, data in enumerate(predicted_data):
         padding = [None for p in range(i * prediction_len)]
